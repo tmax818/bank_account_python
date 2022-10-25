6f0cde2b076792942885884411c69b038defc9fe
tmax818 <tylermaxwell661@gmail.com>
2022-10-25 07:12:51 -0700

    update solution

--- bank_account.py
+++ bank_account.py
@@ -1,22 +1,32 @@
 class BankAccount:
 
-    def __init__(self):
-        pass
+    def __init__(self, interest_rate, balance):
+        self.interest_rate = interest_rate
+        self.balance = balance
 
     def deposit(self, amount):
         """increases the account balance by the given amount"""
-        pass
+        self.balance += amount
+        return self
 
     def withdraw(self, amount):
         """decreases the account balance by the given amount if there are sufficient funds; if there is not enough
         money,print a message: 'Insufficient funds: Charging a $5 fee' and deduct $5 """
-        pass
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print('Insufficient funds: Charging a $5 fee')
+            self.balance -= 5
+        return self
 
     def display_account_info(self):
         """print to the console: eg. 'Balance: $100'"""
-        pass
+        print(f"Balance: ${self.balance}")
+        return self
 
     def yield_interest(self):
         """increases the account balance by the current balance * the interest rate( as long as the balance is
         positive) """
-        pass
+        if self.balance > 0:
+            self.balance += self.balance * self.interest_rate
+        return self
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from bank_account import BankAccount
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Create 2 accounts
+
+acct1 = BankAccount(.1, 1000)
+acct2 = BankAccount(.2, 2000)
+#
+# To the first account, make 3 deposits and 1 withdrawal, then yield interest and display the account's info all in
+# one line of code (i.e. chaining)
+
+acct1.deposit(1000).deposit(1000).deposit(1000).withdraw(2000).yield_interest().display_account_info()
+
+# To the second account, make 2 deposits and 4 withdrawals, then yield interest and display the account's info all in
+# one line of code (i.e. chaining)
+
+acct2.deposit(10000).deposit(10000).withdraw(2000).withdraw(2000).withdraw(2000).withdraw(2000).yield_interest().display_account_info()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
