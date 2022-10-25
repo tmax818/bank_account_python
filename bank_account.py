2f877e96f44b2e558af897641f4668b388bd069b
tmax818 <tylermaxwell661@gmail.com>
2022-10-25 07:01:46 -0700

    update main

--- bank_account.py
+++ bank_account.py
@@ -1,2 +1,22 @@
 class BankAccount:
-    pass
+
+    def __init__(self):
+        pass
+
+    def deposit(self, amount):
+        """increases the account balance by the given amount"""
+        pass
+
+    def withdraw(self, amount):
+        """decreases the account balance by the given amount if there are sufficient funds; if there is not enough
+        money,print a message: 'Insufficient funds: Charging a $5 fee' and deduct $5 """
+        pass
+
+    def display_account_info(self):
+        """print to the console: eg. 'Balance: $100'"""
+        pass
+
+    def yield_interest(self):
+        """increases the account balance by the current balance * the interest rate( as long as the balance is
+        positive) """
+        pass
